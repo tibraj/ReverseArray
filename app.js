285c177d794179b71bee2cbe869627798884ebd6
tibraj <tibraj94@gmail.com>
2021-01-10 12:02:45 -0500

    reverse in place

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ function reverseArray(array) {
 }
 
 console.log(reverseArray([5,7,9,11,15,17,20]));
+
+function reverseInPlace(array) {
+    for(let i = 0; i < Math.floor(array.length / 2); i++) {
+        let original = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = original;
+    }
+    return array;
+}
